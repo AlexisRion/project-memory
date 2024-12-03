6b56679b1353bdf15d2116b509b445fa2b7a6d7f
AlexisRion <kofskin@hotmail.fr>
2024-12-03 15:02:46 +0100

    modified:   js/modules/game.js

--- js/modules/game.js
+++ js/modules/game.js
@@ -104,21 +104,13 @@ function win() {
 }
 
 window.addEventListener("keyup",(event) => {
-    if (event.defaultPrevented) {
-      return; // Do nothing if the event was already processed
-    }
-
-    switch (event.code) {
-      case "Space":
-        restart()
-        console.log("refreshed by the spacebar");
-        break;
-      default:
-        return; // Quit when this doesn't handle the key event.
-    }
-
-    // Cancel the default action to avoid it being handled twice
-    event.preventDefault();
-  },
-  true,
-);
+  switch (event.code) {
+    case "Space":
+      restart()
+      console.log("refreshed by the spacebar");
+      break;
+    default:
+      return; // Quit when this doesn't handle the key event.
+  }
+},
+true);
